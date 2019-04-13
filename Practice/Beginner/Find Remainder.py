05294036092d47a55c5972ce584fb7cd20313d49
Shebin John <admin@remedcu.com>
2019-04-13 21:55:13 +0530

    Added Find Remainder Problem

---
+++ Practice/Beginner/Find Remainder.py
@@ -0,0 +1,10 @@
+#Answer to Find Remainder - https://www.codechef.com/problems/FLOW002
+
+#Read the number of test cases.
+for tc in range(int(input())):
+	# Read integers a and b.
+	(a, b) = map(int, input().split(' '))
+	
+	#Compute the answer
+	#Complete the line below	
+	print(a%b)
